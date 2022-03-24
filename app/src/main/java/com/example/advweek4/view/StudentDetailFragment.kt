ec66c6963803bd3fd302483d9a4c304fcf61b208
adamt <PieceOfCloth>
2022-03-24 10:55:05 +0700

    ANMP_Week7

--- app/src/main/java/com/example/advweek4/view/StudentDetailFragment.kt
+++ app/src/main/java/com/example/advweek4/view/StudentDetailFragment.kt
@@ -6,8 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.advweek4.R
+import kotlinx.android.synthetic.main.fragment_student_detail.*
+import java.util.*
 
 class StudentDetailFragment : Fragment() {
+
+    var student = it
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,5 +19,17 @@ class StudentDetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_student_detail, container, false)
+
+        btnNotif.setOnClickListener {
+            Observable.timer(5, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    Log.d("Messages", "five seconds")
+                    MainActivity.showNotification(student.name.toString(),
+                        "A new notification created",
+                        R.drawable.circle)
+                }
+        }
     }
 }
